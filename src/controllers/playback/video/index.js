657d9dbf745c4058f97d673fd9b8d5d01ec41359
adamanth <adamanth@outlook.com>
2023-04-20 20:13:55 +0300

    Cosmetics

--- src/controllers/playback/video/index.js
+++ src/controllers/playback/video/index.js
@@ -824,13 +824,13 @@ import { PluginType } from '../../../types/plugin.ts';
                     } else {
                         nowPlayingPositionSlider.value = 0;
                     }
+                }
 
-                    if (runtimeTicks && positionTicks != null && currentRuntimeTicks && !enableProgressByTimeOfDay && currentItem.RunTimeTicks && currentItem.Type !== 'Recording' && playbackRate !== null) {
-                        endsAtTime = mediaInfo.getEndsAtFromPosition(runtimeTicks, positionTicks, playbackRate, false);
-                        //endsAtText.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;' + mediaInfo.getEndsAtFromPosition(runtimeTicks, positionTicks, playbackRate, true);
-                    } else {
-                        //endsAtText.innerHTML = '';
-                    }
+                if (nowPlayingPositionSlider && runtimeTicks && positionTicks != null && currentRuntimeTicks && !enableProgressByTimeOfDay && currentItem.RunTimeTicks && currentItem.Type !== 'Recording' && playbackRate !== null) {
+                    endsAtTime = mediaInfo.getEndsAtFromPosition(runtimeTicks, positionTicks, playbackRate, false);
+                    //endsAtText.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;' + mediaInfo.getEndsAtFromPosition(runtimeTicks, positionTicks, playbackRate, true);
+                } else {
+                    //endsAtText.innerHTML = '';
                 }
 
                 if (nowPlayingPositionSlider) {
